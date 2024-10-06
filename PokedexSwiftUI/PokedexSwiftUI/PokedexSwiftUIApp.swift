d06c5333ec2f0105cbada7e9016ef1d8829500cf
Fabrício Rodrigues Pujol <fabriciopujol@MacBook-Pro-de-Fabricio-2.local>
2024-10-06 15:45:56 -0300

    creation of the project

---
+++ PokedexSwiftUI/PokedexSwiftUI/PokedexSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokedexSwiftUIApp.swift
+//  PokedexSwiftUI
+//
+//  Created by Fabricio Pujol on 06/10/24.
+//
+
+import SwiftUI
+
+@main
+struct PokedexSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
